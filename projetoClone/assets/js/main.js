540ce6cc6c3680bea7f26bb1400e35871cc4c266
Mateus F. Gaspar <89864436+MateusFGM@users.noreply.github.com>
2021-10-28 10:12:19 -0300

    Add files via upload

---
+++ projetoClone/assets/js/main.js
@@ -0,0 +1,39 @@
+function projects(){
+  var contador_projects = 0;
+  const incremento_projects = 1;
+  const limite_projects = 654;
+    while(contador_projects<=limite_projects){
+      document.write(contador_projects);
+      contador_projects=contador_projects+incremento_projects;
+    }
+}
+
+function years(){
+  var contador_years = 0;
+  const incremento_years = 1;
+  const limite_years = 23;
+    while(contador_years<=limite_years){
+      document.write(contador_years);
+      contador_years=contador_years+incremento_years;
+    }
+}
+
+function consultants(){
+  var contador_consultants = 0;
+  const incremento_consultants = 1;
+  const limite_consultants = 35;
+    while(contador_consultants<=limite_consultants){
+      document.write(contador_consultants);
+      contador_consultants=contador_consultants+incremento_consultants;
+    }
+}
+
+function successful(){
+  var contador_sucessful = 0;
+  const incremento_sucessful = 1;
+  const limite_sucessful = 836;
+    while(contador_sucessful<=limite_sucessful){
+      document.write(contador_sucessful);
+      contador_sucessful=contador_sucessful+incremento_sucessful;
+    }
+}
